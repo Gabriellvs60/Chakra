93cdb5100b55a0024dc2c8360a3e93a334b5e1ac
gabriellvs60 <gabriellvs60@gmail.com>
2022-08-07 13:11:00 -0300

    feat(utils): internationalization language labels
    Internationalization for the project labels, config language by the browser

---
+++ src/lib/locales/br.ts
@@ -0,0 +1,7 @@
+export const messagesBr = {
+  "label.projectName": "Tradebook",
+  "label.home": "Home",
+  "label.dividends": "Dividendos",
+  "label.b3Stocks": "Ações B3",
+  "label.settings": "Preferências",
+};
