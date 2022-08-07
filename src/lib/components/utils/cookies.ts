afaf14dbb803f6a576f3979e7f87ee64a1f48f8b
gabriellvs60 <gabriellvs60@gmail.com>
2022-08-07 18:56:38 -0300

    build: apollo and codegen config
    n

---
+++ src/lib/components/utils/cookies.ts
@@ -0,0 +1,33 @@
+import { subDays } from "date-fns";
+import type { NextPageContext } from "next";
+import { parseCookies, setCookie as setNookie } from "nookies";
+
+import { config } from "lib/config/env";
+
+export function getCookie(ctx: never | null, key: string) {
+  const cookies = parseCookies(ctx);
+  return cookies?.[`@${config.COOKIE_PREFIX || "traderBook"}:${key}`];
+}
+
+export function setCookie(
+  ctx: Pick<NextPageContext, "res"> | { res: unknown },
+  key: string,
+  value: string,
+  cookieOptions: { maxAge: number; path: string }
+) {
+  return setNookie(
+    ctx,
+    `@${config.COOKIE_PREFIX || "traderBook"}:${key}`,
+    value,
+    cookieOptions
+  );
+}
+
+export function destroyCookie(key: string) {
+  const expireDate = subDays(new Date(), 1);
+
+  // eslint-disable-next-line no-return-assign
+  return (document.cookie = `@${
+    config.COOKIE_PREFIX || "traderBook"
+  }:${key} = null; path=/; expires=${expireDate};`);
+}
